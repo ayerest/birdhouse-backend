4d9b0d4a9fa13f65499913ddb5304a09094b4d8c
ayerest <irisiseve@gmail.com>
2019-10-09 17:51:17 -0700

    worked on bringing bird data to the front end

--- BirdHouse/config/routes.rb
+++ BirdHouse/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :bird_images
   resources :badges
   resources :bird_entries
-  resources :birds
+  get '/birds' => 'birds#index'
+
+  # resources :birds, except: [:index]
   resources :images
   resources :field_entries
   resources :users
