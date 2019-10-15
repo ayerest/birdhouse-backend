7c6b229dffc2f3cd79f668e521eddf10a9ed859e
ayerest <irisiseve@gmail.com>
2019-10-15 10:53:11 -0700

    thinned out info coming back from bird fetches to improve performance

--- BirdHouse/app/controllers/bird_entries_controller.rb
+++ BirdHouse/app/controllers/bird_entries_controller.rb
@@ -3,6 +3,9 @@ class BirdEntriesController < ApplicationController
     def filter_birds
         searchTerm = params[:searchTerm]
         birds = Bird.filter_birds(searchTerm)
+        birds = birds.map do |bird|
+            {common_name: bird.common_name, species_name: bird.species_name, img_url: bird.img_url, id: bird.id, birdcall: bird.birdcall}
+        end
         render :json => birds
     end
 end
--- BirdHouse/app/controllers/bird_images_controller.rb
+++ BirdHouse/app/controllers/bird_images_controller.rb
@@ -3,6 +3,9 @@ class BirdImagesController < ApplicationController
     def get_my_birds
         user = User.find((params[:user][:id]))
         birds = user.my_birds
+        birds = birds.map do |bird|
+            {common_name: bird.common_name, species_name: bird.species_name, img_url: bird.img_url, id: bird.id, birdcall: bird.birdcall}
+        end
         render :json => birds
     end
 
