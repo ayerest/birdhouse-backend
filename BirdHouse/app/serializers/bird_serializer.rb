a082433c100a7251679abd957372b19103a097d2
ayerest <irisiseve@gmail.com>
2019-10-22 14:10:15 -0700

    added citation to bird serializer

--- BirdHouse/app/serializers/bird_serializer.rb
+++ BirdHouse/app/serializers/bird_serializer.rb
@@ -1,3 +1,3 @@
 class BirdSerializer < ActiveModel::Serializer
-  attributes :id, :common_name, :species_name, :img_url, :birdcall, :range_map, :details, :category, :field_entries, :bird_images, :images, :quick_info
+  attributes :id, :common_name, :species_name, :img_url, :birdcall, :range_map, :details, :citation, :category, :field_entries, :bird_images, :images, :quick_info
 end
